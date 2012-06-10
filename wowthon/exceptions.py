11d7a21304b32e0a6db0209c12a8a3d6144f4938
Jonathan Goodger <jonno.is@gmail.com>
2012-06-10 19:44:47 +0100

    Implemented APIError

--- wowthon/exceptions.py
+++ wowthon/exceptions.py
@@ -1,8 +1,15 @@
 class APIError(Exception):
+    """
+    Raised when an invalid API request is made.
+    
+    """
     def __init__(self, code, json):
         self.code = code
         self.status = json['status']
         self.reason = json['reason']
         
     def __str__(self):
-        return self.code + ': ' + self.reason
+        return str(self.code) + ': ' + self.reason
+        
+    def __repr__(self):
+        return 'APIError <' + str(self) + '>'
